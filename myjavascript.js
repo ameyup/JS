75c1c353e5d4a44457bdd8bf4565a0e4935bb8f1
ameyup <ameyup@gmail.com>
2020-12-28 15:54:48 +0530

    JS project. Completed basic JS certification

---
+++ myjavascript.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+	<head>
+		<title>
+			<script>
+				document.write("Hello World from JS!!");
+			</script>
+		</title>
+	</head>
+	<body>
+		<script>
+			document.write("Hello World JSBody!!");			
+		</script>
+	</body>
+</html>
